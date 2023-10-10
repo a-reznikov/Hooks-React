e5aa10a399efede54e3f5847c5ca42b320ca6260
a-reznikov <a.reznnikov@gmail.com>
2023-10-10 10:26:00 +0300

    feat: useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+
+  const [color, setColor] = useState('gray');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{ padding: '10px', backgroundColor: color, fontSize: `${fontSize}px` }}>
+      Hooks
+      <button className='btn btn-dark' onClick={() => setColor('gray')}>Dark</button>
+      <button className='btn btn-light' onClick={() => setColor('white')}>Light</button>
+      <button className='btn' onClick={() => setFontSize((size) => size + 2)}>+</button>
+
+    </div>
+  )
+}
+
+
+
+
 root.render(
-  <h1>Start new project</h1>
+  <App />
 );
 
